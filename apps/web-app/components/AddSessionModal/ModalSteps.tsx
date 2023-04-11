91d452bc0c7940ce925cdfdef679f005e34620bf
m <13090422+marciob@users.noreply.github.com>
2023-04-11 14:04:49 +0900

    Changes

--- apps/web-app/components/AddSessionModal/ModalSteps.tsx
+++ apps/web-app/components/AddSessionModal/ModalSteps.tsx
@@ -4,21 +4,10 @@ type Props = {
 
 const ModalSteps = ({ steps }: Props) => (
     <div className="flex justify-start gap-3 w-full my-10">
-        <div
-            className={`w-[10px] h-[10px] rounded-full ${
-                steps >= 1 ? "bg-zulalu-primary" : "bg-[#D3DDDC]"
-            }`}
-        />
-        <div
-            className={`w-[10px] h-[10px] rounded-full ${
-                steps >= 2 ? "bg-zulalu-primary" : "bg-[#D3DDDC]"
-            }`}
-        />
-        <div
-            className={`w-[10px] h-[10px] rounded-full ${
-                steps >= 3 ? "bg-zulalu-primary" : "bg-[#D3DDDC]"
-            }`}
-        />
+        <div className={`w-[10px] h-[10px] rounded-full ${steps >= 1 ? "bg-zulalu-primary" : "bg-[#D3DDDC]"}`} />
+        <h1>dasa</h1>
+        <div className={`w-[10px] h-[10px] rounded-full ${steps >= 2 ? "bg-zulalu-primary" : "bg-[#D3DDDC]"}`} />
+        <div className={`w-[10px] h-[10px] rounded-full ${steps >= 3 ? "bg-zulalu-primary" : "bg-[#D3DDDC]"}`} />
     </div>
 )
 
